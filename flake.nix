a061f6da09d9cefabbec814ff311053f352355db
Maxwell Spadafore <maxspad@umich.edu>
2024-01-28 13:46:08 -0500

    initial commit

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "My first flake";
+
+  inputs = {
+   nixpkgs.url = "nixpkgs/nixos-23.11";
+  };
+
+  outputs = { self, nixpkgs, ...}:
+    let
+      lib = nixpkgs.lib;
+    in {
+      nixosConfigurations = {
+       nixos = lib.nixosSystem {
+         system = "aarch64-linux";
+   	 modules = [ ./configuration.nix ];
+       };
+      };
+    };
+}
