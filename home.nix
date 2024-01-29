845b7099c75cfb7187190f6a35c074bb0495af4b
Maxwell Spadafore <maxspad@umich.edu>
2024-01-29 11:30:59 -0500

    added basic programs

--- home.nix
+++ home.nix
@@ -7,4 +7,54 @@
 
   # let home-manager install and enable itself
   programs.home-manager.enable = true;
+
+  home.packages = with pkgs; [
+	neofetch
+	nnn
+
+	#archives
+	zip
+	xz
+	unzip
+	p7zip
+
+	#utils
+	ripgrep
+	jq
+	yq-go
+	eza
+	fzf
+
+	#networking
+	mtr
+	dnsutils
+	ldns
+	
+	#misc
+	cowsay
+	file
+	which
+	tree
+	gnused
+	gnutar
+	gawk
+	zstd
+	gnupg
+
+	# nix-related
+	nix-output-monitor # provides "nom" for nix but with more details
+
+	# system tools
+	btop # replaces htop
+	iotop
+	iftop
+	strace
+	ltrace
+	lsof # list open files
+	sysstat
+	lm_sensors # for "sensors" command
+	ethtool
+	pciutils # lspci
+	usbutils # lsusb
+  ];
 }
